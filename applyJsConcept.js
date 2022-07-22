7f150fc24278ec14f61d42a6b51fb59c8183d7aa
jaber4770 <jaber.ahmed4770@gmail.com>
2022-07-22 16:14:22 +0600

    mile to kmn done

--- applyJsConcept.js
+++ applyJsConcept.js
@@ -16,7 +16,7 @@ let moonUP = isMoonUp(checkTime);
 console.log(moonUP);
 
 
-// inch to feet
+// inch to feet convert____inch to feet convert____inch to feet convert____
 
 function inchToFeet(inch){
     let feet = inch / 12;
@@ -27,4 +27,18 @@ function inchToFeet(inch){
 let inch = 150;
 let feetResult = inchToFeet(inch);
 let integerNumber = feetResult.toFixed(2);
-console.log(integerNumber);
+console.log(integerNumber,"Feet");
+
+
+
+// Mile to Kilo Meter______Mile to Kilo Meter______Mile to Kilo Meter______
+
+function mileToKM(mile){
+    let Kilometer = mile * 1.60934;
+    return Kilometer;
+}
+
+let mile = 20;
+let getKmResult = mileToKM(mile);
+let getFinalKmResult = getKmResult.toFixed(2);
+console.log(getFinalKmResult,"km");
