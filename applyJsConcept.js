5db437a141b325809141b5dbd8c1a7b5db66a41f
jaber4770 <jaber.ahmed4770@gmail.com>
2022-07-22 15:47:57 +0600

    checkin moon

--- applyJsConcept.js
+++ applyJsConcept.js
@@ -1,1 +1,16 @@
-// let's start applying js conect
+// let's start applying js conecpt
+// cheing moon up or not
+
+
+
+function isMoonUp(time) {
+    if (time >= 19 && time <= 5) {
+        return true;
+    }
+    return false;
+}
+
+
+let checkTime = 21;
+let moonUP = isMoonUp(checkTime);
+console.log(moonUP);
