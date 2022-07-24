5136f79ee56a71df2b93a6f7f165b87c49121970
jaber4770 <jaber.ahmed4770@gmail.com>
2022-07-24 11:22:54 +0600

    exam grade system added

--- applyJsConcept.js
+++ applyJsConcept.js
@@ -211,6 +211,52 @@ console.log(getCelciusResult);
 
 
 
+// Grade in exam_____Grade in exam_____Grade in exam_____Grade in exam_____Grade in exam
+
+// Letter grade;
+
+function ExamGrade(marks) {
+    if (marks >= 100) {
+        console.log("Exam marks can not be more than 100");
+    }
+    else if (marks <= 0) {
+        console.log("Exam marks can not be less than 0");
+    }
+    else if (marks >= 80 && marks <= 100) {
+        console.log("You got A+");
+    }
+    else if (marks >= 70 && marks <= 79) {
+        console.log("You got A");
+
+    }
+    else if (marks >= 60 && marks <= 69) {
+        console.log("You got A-");
+
+    }
+    else if (marks >= 50 && marks <= 59) {
+        console.log("You got B");
+
+    }
+    else if (marks >= 40 && marks <= 49) {
+        console.log("You got C");
+
+    }
+    else if (marks >= 33 && marks <= 39) {
+        console.log("You got D");
+
+    }
+    else if (marks >= 0 && marks <= 32) {
+        console.log("you got Fail")
+    }
+    else {
+        console.log("You didn't attend in the exam");
+    }
+}
+
+
+let studentMarks = 95;
+ExamGrade(studentMarks);
+
 
 
 
